bbc04b3aa3abcd9e81776c88f32af1f1eb1f56a8
johnnywhite612 <johnnywhite612@gmail.com>
2019-10-18 11:38:44 +0100

    Final tests added for my BST Lowest Common Ancestor alogrithm and data structure

--- BST.js
+++ BST.js
@@ -1,10 +1,13 @@
 export default class BinarySearchTree {
 
+    // This sets up my BST and initialises the root to null
     constructor() {
         this.root = null;
     }
 
+    // This method inserts a new node into the tree
     insertNumberNode(data, left = null, right = null) {
+        // Defining the node structure
         let Node = {
             data,
             left,
@@ -13,9 +16,10 @@ export default class BinarySearchTree {
 
         let currentNode;
 
-        if (!this.root) {
+        if (!this.root) {        // If the root node is null, initialise the root node as the node just inserted
             this.root = Node;
-        } else {
+        } else {    // Otherwise propogate the node down the tree until a suitable spot is found
+
             currentNode = this.root;
             while (currentNode) {
                 if (data < currentNode.data) {
@@ -41,28 +45,28 @@ export default class BinarySearchTree {
     }
 
 
-    // search for a node with given data 
+    // Search for a node with the given data 
     search(node, data) {
-        // if trees is empty return null 
-        if (node === null)
+        // If the Tree is empty then return null 
+        if (node === null){
             return null;
+        }
 
-        // if data is less than node's data 
-        // move left 
-        else if (data < node.data)
+        // If data is less than node's data, branch left
+        else if (data < node.data){
             return this.search(node.left, data);
-
-        // if data is less than node's data 
-        // move left 
-        else if (data > node.data)
+        }
+        // If the data is less than node's data, branch right 
+        else if (data > node.data){
             return this.search(node.right, data);
-
-        // if data is equal to the node data  
-        // return node 
-        else
+        }
+        // If the data is equal to the node data, then return the node
+        else{
             return node;
+        }
     }
 
+    //Is this value valid / does it exist in my BST??
     isValidValue(node, val) {
         if (node) {
             if (this.search(node, val) === null) {
@@ -74,29 +78,31 @@ export default class BinarySearchTree {
         return false;
     }
 
+
     findLCA(node, n1, n2) {
-        //Check if the node supplied is null
+        // Check if the node supplied is null
         if (node === null) {
             return null;
         }
-        //Check if the input values actually exist in the BST
+        // Check if the input values actually exist in the BST
         if (!this.isValidValue(node, n1) || !this.isValidValue(node, n2)) {
             return null;
         }
         if (n1 === n2) {
             return null;
         }
-        //Check if inputs exceed the max value
+        // Check if inputs exceed the max value
         if (n1 > Number.MAX_VALUE || n2 > Number.MAX_VALUE) {
             return null;
         }
-        //Check if inputs exceed the min value
+        // Check if inputs exceed the min value
         if (n1 < Number.MIN_VALUE || n2 < Number.MIN_VALUE) {
             return null;
         }
-        return this.lowestCommonAncestor(node, n1, n2);
+            return this.lowestCommonAncestor(node, n1, n2);
     }
 
+    // The method for finding the LCA in the BST - called recursively
     lowestCommonAncestor(node, n1, n2) {
         if (!node) return;
 
--- BST.test.js
+++ BST.test.js
@@ -1,20 +1,23 @@
 import BinarySearchTree from './BST';
 
 it('Simple working tests', () => {
+
+  //Initialise our BST data structure
   let BST = new BinarySearchTree();
 
+  //Insert some nodes for testing
   BST.insertNumberNode(7);
   BST.insertNumberNode(14);
   BST.insertNumberNode(5);
-  BST.insertNumberNode(13); 
+  BST.insertNumberNode(13);
   BST.insertNumberNode(8);
   BST.insertNumberNode(18);
   BST.insertNumberNode(15);
   BST.insertNumberNode(6);
   BST.insertNumberNode(29);
   BST.insertNumberNode(42);
-  //Insert in again, our system won't add it to the BST
-  BST.insertNumberNode(42);
+  BST.insertNumberNode(-10);
+  BST.insertNumberNode(-15);
 
   /*
   BST Looks like the following
@@ -22,26 +25,52 @@ it('Simple working tests', () => {
             /   \
           5     14
         / \     /  \
-           6   13    18
-              / \   /  \ 
-             8     15   29
+      -10   6   13    18
+      /        / \   /  \ 
+    -15       8     15   29
                           \
                            42
-
   */
-  
+
+  // ** First Test **
+  // Find the LCA of two nodes at the same depth in the BST
   let lca = BST.findLCA(BST.root, 13, 18);
   expect(lca.data).toBe(14);
+
+  // ** Second Test **
+  // Find the LCA of two nodes in different branches and at different depths in the BST
   let lca2 = BST.findLCA(BST.root, 8, 6);
   expect(lca2.data).toBe(7);
-  let lca3 = BST.findLCA(BST.root, 29, 15);
+
+  // ** Third Test **
+  // Find LCA of two nodes with a LCA at a similar depth
+  let lca3 = BST.findLCA(BST.root, 42, 15);
   expect(lca3.data).toBe(18);
+
+  // ** Fourth Test **
+  // Find LCA of two nodes with the root node as the LCA
+  // Both nodes are at different depths and in different branches, 
+  // this will test the algorithms accuracy accross nodes which are quite unrelated
   let lca4 = BST.findLCA(BST.root, 42, 6);
   expect(lca4.data).toBe(7);
+
+  // ** Fifth Test **
+  // Find LCA of two nodes where one node is the parent of the other
   let lca5 = BST.findLCA(BST.root, 6, 5);
   expect(lca5.data).toBe(5);
-});
 
+  // ** Sixth Test **
+  // Find LCA of two nodes where one has a negative value
+  // This test fails as my algorithm can't work with negative values
+  let lca6 = BST.findLCA(BST.root, -10, 13);
+  expect(lca6.data).toBe(7);
+
+  // ** Seventh Test **
+  // Find LCA of two nodes where both have negative values
+  // This test fails as my algorithm can't work with negative values
+  let lca6 = BST.findLCA(BST.root, -10, -15);
+  expect(lca6.data).toBe(-10);
+});
 
 it('Test for keys that don\'t exist', () => {
   let BST = new BinarySearchTree();
@@ -60,26 +89,37 @@ it('Test for keys that don\'t exist', () => {
         / \     /  \
                12    33
   */
-  
+
+  // ** First Test **
+  // Find the LCA of two nodes; both don't exist in the BST
   let lca = BST.findLCA(BST.root, 0, 3);
   expect(lca).toBe(null);
+
+  // ** Second Test **
+  // Find the LCA of two nodes; only one exists in the BST
   let lca2 = BST.findLCA(BST.root, 33, 3);
   expect(lca2).toBe(null);
+
+  // ** Third Test **
+  // Find the LCA of two nodes; both don't exist and they both have either large or negative values
   let lca3 = BST.findLCA(BST.root, -323, 2113);
   expect(lca3).toBe(null);
-  //Here we'll test a value that exists in our BST and one that doesn't
+
+  // ** Fourth Test **
+  // Find the LCA of two nodes; one exists and one is a minus value that doesn't exist
   let lca4 = BST.findLCA(BST.root, -23, 28);
   expect(lca4).toBe(null);
+
 });
 
-  
+
 it('Test for identical values searched', () => {
   let BST = new BinarySearchTree();
 
   BST.insertNumberNode(7);
   BST.insertNumberNode(14);
   BST.insertNumberNode(5);
-  BST.insertNumberNode(13); 
+  BST.insertNumberNode(13);
   BST.insertNumberNode(8);
   BST.insertNumberNode(18);
   BST.insertNumberNode(15);
@@ -97,20 +137,20 @@ it('Test for identical values searched', () => {
               / \   /  \ 
              8     15   29
   */
-  
+
   let lca = BST.findLCA(BST.root, 13, 13);
   expect(lca).toBe(null);
 
 });
 
 
-it('Test for unexpected input', () => {
+it('Test for edge cases', () => {
   let BST = new BinarySearchTree();
 
   BST.insertNumberNode(7);
   BST.insertNumberNode(14);
   BST.insertNumberNode(5);
-  BST.insertNumberNode(13); 
+  BST.insertNumberNode(13);
   BST.insertNumberNode(8);
   BST.insertNumberNode(18);
   BST.insertNumberNode(15);
@@ -130,14 +170,13 @@ it('Test for unexpected input', () => {
                           \
                            42
   */
-  
-//Test null node input
-//Test exceeding min val
 
- let exceedMaxVal = Number.MAX_VALUE * 2;
- let exceedMinVal = Number.MIN_VALUE / -100;
+  // ** Testing inputs exceeding max and min values **
 
-//Test with input exceeding max val
+  let exceedMaxVal = Number.MAX_VALUE * 2;
+  let exceedMinVal = Number.MIN_VALUE / -100;
+
+  //Test with input exceeding max val
   let lca = BST.findLCA(BST.root, exceedMaxVal, 29);
   expect(lca).toBe(null);
 
@@ -145,7 +184,7 @@ it('Test for unexpected input', () => {
   let lca2 = BST.findLCA(BST.root, 15, exceedMinVal);
   expect(lca2).toBe(null);
 
-  //Test with null root node
+  // ** Testing inputs with a null root node **
   let lca3 = BST.findLCA(null, 13, 6);
   expect(lca3).toBe(null);
 
